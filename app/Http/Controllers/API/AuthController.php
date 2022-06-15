999958c1bc5a04fb7488f5e292e12e1f59c827a3
mattbu <chillysaucing@gmail.com>
2022-06-16 01:33:36 +0900

    cors 에러 해결 및 회원가입시 유저이미지 path DB에 저장

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 use Laravel\Passport\Client;
 use Symfony\Component\HttpFoundation\Response;
 use App\Models\User;
@@ -27,26 +28,32 @@ class AuthController extends Controller
             ], Response::HTTP_BAD_REQUEST);
         }
 
-        $data = request()->only('email', 'name', 'password');
+        $data = request()->only('email', 'name', 'password', 'avatar_img');
 
+        if ($request->file('avatar_img')->isValid()) {
+            $avatar_img = $request->file('avatar_img')->store('public/images');
+            $filename = $request->file('avatar_img')->getClientOriginalName();
+            $image_path = Storage::url($avatar_img);
+        }
+        
         // 사용자 생성
-        $user = \App\Models\User::create([
+        $user = User::create([
            'name' => $data['name'],
            'email' => $data['email'],
            'password' => bcrypt($data['password']),
+           'avatar_img' => $image_path
         ]);
 
-//        $token = $this->createToken($data['email'], $data['password']);
         $token = $user->createToken('access-token');
         return response([
-           'token' => $token,
+           'token' => $token->accessToken,
            'message' => 'success'
         ], 201);
     }
 
 
     public function login(Request $request) {
-        $user = User::where('email',$request->user_id)->first();
+        $user = User::where('email', $request->user_id)->first();
 
         abort_unless($user, 403, '이메일 또는 비밀번호를 다시 확인해 주세요.');
         abort_unless(
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('avatar_img')->nullable();
+            // $table->rememberToken();
             $table->timestamps();
         });
     }
